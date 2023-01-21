cd67d2ab4ff12959a27631b68ec091ee46ec521f
TRK41 <Troyk@bell.net>
2023-01-21 13:03:36 -0500

    adding data to classes

---
+++ engineer.js
@@ -0,0 +1,16 @@
+const Employee = require('./employee');
+
+class Engineer extends Employee {
+  constructor(github){
+    super(name,id,email);
+    this.github = github;
+  }
+
+  getGithub() {
+    console.log(this.github);
+  }
+
+  getRole(){
+    return Engineer
+  }
+}
